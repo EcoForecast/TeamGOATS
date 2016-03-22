be786781a46e73326a7c585ded052b150952d099
PEcAn Demo User <ajpineda@bu.edu>
2016-03-22 10:20:15 -0500

    yes

--- get_googletrends_data.R
+++ get_googletrends_data.R
@@ -1,3 +1,8 @@
+#!/usr/bin/env Rscript
+.libPaths(c("/home/carya/R/library",.libPaths()))
+.libPaths()
+setwd("/home/carya/TeamGOATS")
+
 #install gtrendsR package that pulls Google trend data
 install.packages("gtrendsR")
 library(gtrendsR)
@@ -12,3 +17,10 @@ zika_trend=gtrends("zika",geo=c("CO"), start_date= "2015-09-01")
 plot(zika_trend)
 
 
+#check if the cron job is running
+file_before= file("GoogleTrendsCronLastUpdate")
+cat("JAG IS UP AND RUNNING",date(),file= file_before, append=TRUE)
+close(file_before)
+
+save.image("googleZika.RData")
+#save
