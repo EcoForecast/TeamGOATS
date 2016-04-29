f8cef31f3bf689b1250cd09084f30ed838b8379e
Stephen Caron <caronst@bu.edu>
2016-04-29 15:42:13 -0400

    Dietze's fixes and addition of Kalman filter

--- initial_forecast.R
+++ initial_forecast.R
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-i=5 #department (choose whichever has most/best data)
-rand=sample.int(nmc,nmcmc)
-
-for (k in 1:nmn){
-  m=rand[k]
-  xf[t,i,k] = x[time] #setting initial conditions --> first time from MCMC, later from analysis
-  for(t in start:end){
-    z[t,i,k] = xf[t,i,k]+ r[m] + dept[i,m]
-    xf[t,i,k] = rnorm(z[t,i,k],tau_add[m])
-  }
-=======
 #i=1 #department (choose whichever has most/best data)
-
-nmc = 5000 # 1000 to 5000
+library(mvtnorm)
+nmc = 500 # 1000 to 5000
 nmcmc = nrow(out) # = rows in out
 rand=sample.int(nmcmc,nmc)
 start = 8
@@ -21,11 +9,11 @@ end=10
 xf = array(NA,dim = c(end,36,nmc))
 
 for(i in 1:36){  
-x = out[,grep("x[7,1]",colnames(out),fixed=TRUE)] #fix so this "x[7,1]" is not hard coded
+x = out[,grep(paste0("x[7,",i,"]"),colnames(out),fixed=TRUE)] #fix so this "x[7,1]" is not hard coded
 r = out[,grep("r",colnames(out),fixed=TRUE)]
-dept = out[,grep("dept[1]",colnames(out),fixed=TRUE)]
-tau_add = out[,grep("tau_add",colnames(out),fixed=TRUE)]
-  
+dept = out[,grep(paste0("dept[",i,"]"),colnames(out),fixed=TRUE)]
+tau_add = 1/sqrt(out[,grep("tau_add",colnames(out),fixed=TRUE)])
+tau_obs = 1/sqrt(out[,grep("tau_obs",colnames(out),fixed=TRUE)])  
 
    for (k in 1:nmc){
       m=rand[k]
@@ -35,34 +23,60 @@ tau_add = out[,grep("tau_add",colnames(out),fixed=TRUE)]
         xf[t,i,k] = rnorm(1,z,tau_add[m])
         }
    }
-<<<<<<< HEAD
-
-ci = apply(xf[,i,],2,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
-plot(time,ci[2,(start:end)+(i-1)*7],xlab="Time",ylab="Estimated Cases",
-     main=colnames(dept.total[i]),ylim=range(pi[,(start:end)+(i-1)*7]))
-ciEnvelope(time,ci[1,(start:end)+(i-1)*7],ci[3,(start:end)+(i-1)*7],col="Blue")
-points(time,ci[2,(start:end)+(i-1)*7],ylab="Estimated Cases")
->>>>>>> b31a2dd0a0a34fdb1ec822a163f72a2ee15c1d55
-=======
->>>>>>> 8a804b02200127c6e7f0675e74dda04ddf45560c
 }
 
+#data=apply(exp(out[,grep("x",colnames(out))]),2,quantile,c(0.025,0.5,0.975))
 x=seq(1,end,1) # Time vector 
-ci=array(NA,dim=c(3,end,36))
+ci.f=array(NA,dim=c(3,end,36))
 for(i in 1:36){
-ci[,,i] <- apply(exp(xf[,i,]),1,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
-plot(ci[2,,i],ylim=range(ci[,,i],na.rm=TRUE),xlab="Day",ylab="Total Cases",main=colnames(dept.total[i]))
-ciEnvelope(x,ci[1,,i],ci[3,,i],col="lightBlue")
-points(ci[2,,i])
+ci.f[,,i] <- apply(exp(xf[,i,]),1,quantile,c(0.025,0.5,0.975),na.rm=TRUE)
+plot(ci.f[2,,i],ylim=range(ci.f[,,i],na.rm=TRUE),xlab="Week",ylab="Total Cases",main=colnames(dept.total[i]))
+ciEnvelope(x,ci.f[1,,i],ci.f[3,,i],col="lightBlue")
+points(ci.f[2,,i])
+}
+
+time.f=seq(1,end,1)
+for(i in 1:36){
+  ylim=range(cbind(ci[,(1:7)+(i-1)*7],ci.f[,,i]),na.rm=TRUE)
+  plot(time.f,c(ci[2,(1:7)+(i-1)*7],ci.f[2,start:end,i]),xlab="Time",ylab="Zika Index",main=colnames(dept.total[i]),log="y",ylim=ylim,na.rm=TRUE)
+  #ciEnvelope(time,pi[1,(1:7)+(i-1)*7],pi[3,(1:7)+(i-1)*7],col="lightBlue")
+  ciEnvelope(time.f[1:(start-1)],pi[1,(1:7)+(i-1)*7],pi[3,(1:7)+(i-1)*7],col="lightBlue")
+  #ciEnvelope(time.f,c(ci[1,(1:7)+(i-1)*7],ci.f[1,,i]),c(ci[3,(1:7)+(i-1)*7],ci.f[3,,i]),col="Blue")
+  ciEnvelope(time.f,ci.f[1,,i],ci.f[3,,i],col="lightGreen")
+  points(time.f,c(ci[2,(1:7)+(i-1)*7],ci.f[2,start:end,i]))
+  points(ci.f[2,,i],pch="+",col="darkGreen")
 }
 
 #sensitivity and uncertainty 
 
+#pseudodata
+Y = matrix(NA,36,end)
+for(i in 1:36){
+  Y[i,] = xf[,i,sample.int(nmc,1)]
+}
+
 ## do analysis
-muf = mean()
-Pf = var()
-KF Math
-x = sample from rnorm(ne,mua,pa)
+mu.f = apply(xf[start,,],1,mean)
+P.f = var(t(xf[start,,]))
+I = diag(1,36)
+R = diag(mean(tau_obs),36)
+#KF Math
+
+## Analysis step: combine previous forecast with observed data
+obs = !is.na(Y[,start]) ## which Y's were observed?
+if(any(obs)){
+  H <- I[obs,]                                                        ## observation matrix
+  K <- P.f %*% t(H) %*% solve(H%*%P.f%*%t(H) + R[obs,obs])  ## Kalman gain
+  mu.a <- mu.f + K%*%(Y[obs,start] - H %*% mu.f)              ## update mean
+  P.a <- (1-K %*% H)*P.f                                  ## update covariance
+} else {
+  ##if there's no data, the posterior is the prior
+  mu.a = mu.f
+  P.a = P.f
+}
+
+
+x = rmvnorm(nmc,mu.a,P.a)
 
 #note: modeling x on a log scale
 
